c9fd74fc54bd9d3d92441912f23d508c96deb4d5
Zoran1967 <122949943+Zoran1967@users.noreply.github.com>
2026-08-18 14:41:11 +0200

    Fix indentation error in recordings.py

--- screens/recordings.py
+++ screens/recordings.py
@@ -59,7 +59,7 @@ class RecordingsScreen(MDScreen):
         try:
             entries = []
             entries += self._collect(_app_dir("recordings"), (".m4a",), "Snimljeno")
-            entries += self._collect(
+                        entries += self._collect(
                 _app_dir("imported"), (".mp3", ".wav"), "Ucitano"
             )
             entries.sort(key=lambda e: e[2], reverse=True)  # newest first
